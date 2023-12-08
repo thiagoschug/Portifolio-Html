b6f0bd247de19d6b6e422c43823d878b10ad7c0f
thiagoschug <thiagoschug98@gmail.com>
2023-12-07 21:49:46 -0300

    Ultimo commit

---
+++ reveal.js
@@ -0,0 +1,56 @@
+window.revelar = ScrollReveal({reset:true})
+
+//SUPORTE E MANUTENÇÃO
+revelar.reveal('.efeito-txt',
+{
+ duration: 2000,
+ distance: '100px'
+})
+
+revelar.reveal('.efeito-img',
+{
+ duration: 2000,
+ distance: '100px',
+ delay: 500
+})
+
+//DEV WEB
+revelar.reveal('.efeito-txtdev',
+{
+ duration: 2000,
+ distance: '100px',
+ origin: 'left'
+})
+
+revelar.reveal('.efeito-imgdev',
+{
+ duration: 2000,
+ distance: '100px',
+ delay: 500,
+ origin: 'right'
+})
+
+//CONSULTORIA
+revelar.reveal('.efeito-txtconsultoria',
+{
+ duration: 2000,
+ distance: '100px',
+ origin: 'right'
+
+})
+
+revelar.reveal('.efeito-imgconsultoria',
+{
+ duration: 2000,
+ distance: '100px',
+ origin: 'left',
+ delay: 500
+})
+
+//FINAL DO SITE
+revelar.reveal('.efeito-txtfinalsite',
+{
+ duration: 2000,
+ distance: '100px',
+ origin: 'bottom'
+})
--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 window.addEventListener('scroll' , function(){
  var header = document.querySelector('#header')
- header.classList.toggle('rolagem', window.scrollY > 500)
+ header.classList.toggle('rolagem', window.scrollY > 0)
 })
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Adiciona um ouvinte de eventos de clique a todos os links da navegação
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            // Obtém o destino do link clicado
+            const targetId = this.getAttribute('href').substring(1);
+
+            // Rola suavemente para a seção de destino
+            document.getElementById(targetId).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
+

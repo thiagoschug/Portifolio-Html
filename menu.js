010692ff3fc82827524e6c24aff23f6241cde6bf
thiagoschug <thiagoschug98@gmail.com>
2023-12-22 19:50:57 -0300

    finalizandosite

--- menu.js
+++ menu.js
@@ -1,23 +1,12 @@
-let btnabmenu = document.getElementById('btn-abrirmenu');
-let btnfecharmenu = document.getElementById('btn-fechar');
-let menu = document.getElementById('menu-mobile');
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
 
-btnabmenu.addEventListener('click', () => {
-   menu.classList.add('abrir-menumb');
-});
 
-btnfecharmenu.addEventListener('click', (event) => {
-   // Impedir que o clique no botão de fechar propague para fechar o menu
-   event.stopPropagation();
-   menu.classList.remove('abrir-menumb');
-});
 
-menu.addEventListener('click', (event) => {
-   // Impedir que o clique no conteúdo do menu propague para fechar o menu
-   event.stopPropagation();
-});
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrirmeumenu')
+})
 
-document.addEventListener('click', () => {
-   // Fechar o menu se clicar fora dele
-   menu.classList.remove('abrir-menumb');
-});
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrirmeumenu')
+})
--- reveal.js
+++ reveal.js
@@ -1,56 +1,48 @@
-window.revelar = ScrollReveal({reset:true})
+window.revelar = ScrollReveal({ reset: true });
 
 //SUPORTE E MANUTENÇÃO
-revelar.reveal('.efeito-txt',
-{
- duration: 3000,
- distance: '100px'
-})
+revelar.reveal(".efeito-txt", {
+  duration: 3000,
+  distance: "100px",
+});
 
-revelar.reveal('.efeito-img',
-{
- duration: 3000,
- distance: '100px',
- delay: 500
-})
+revelar.reveal(".efeito-img", {
+  duration: 3000,
+  distance: "100px",
+  delay: 500,
+});
 
 //DEV WEB
-revelar.reveal('.efeito-txtdev',
-{
- duration: 2000,
- distance: '100px',
- origin: 'left'
-})
+revelar.reveal(".efeito-txtdev", {
+  duration: 2000,
+  distance: "100px",
+  origin: "left",
+});
 
-revelar.reveal('.efeito-imgdev',
-{
- duration: 2000,
- distance: '100px',
- delay: 500,
- origin: 'right'
-})
+revelar.reveal(".efeito-imgdev", {
+  duration: 2000,
+  distance: "100px",
+  delay: 500,
+  origin: "right",
+});
 
 //CONSULTORIA
-revelar.reveal('.efeito-txtconsultoria',
-{
- duration: 2000,
- distance: '100px',
- origin: 'right'
+revelar.reveal(".efeito-txtconsultoria", {
+  duration: 2000,
+  distance: "100px",
+  origin: "right",
+});
 
-})
-
-revelar.reveal('.efeito-imgconsultoria',
-{
- duration: 2000,
- distance: '100px',
- origin: 'left',
- delay: 500
-})
+revelar.reveal(".efeito-imgconsultoria", {
+  duration: 2000,
+  distance: "100px",
+  origin: "left",
+  delay: 500,
+});
 
 //FINAL DO SITE
-revelar.reveal('.efeito-txtfinalsite',
-{
- duration: 2000,
- distance: '100px',
- origin: 'bottom'
-})
+revelar.reveal(".efeito-txtfinalsite", {
+  duration: 2000,
+  distance: "100px",
+  origin: "bottom",
+});
